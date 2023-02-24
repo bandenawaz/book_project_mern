9b01440f91866c9f34f813093cb771fbdff986fd
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-02-24 20:46:19 +0530

    Frontend Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<ShowBookList />} />
+          <Route path='/createBook' element={<CreateBook />} />
+          <Route path='/editBook' element={<UpdateBookInfo />} />
+          <Route path='/showBook/:id' element={<ShowBookDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
